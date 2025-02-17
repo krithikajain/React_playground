7d80ba33ae2640e04ed2d3281bc66641139962da
Krithika Jain <krithikajain@Krithikas-MacBook-Air.local>
2025-02-17 00:03:37 -0500

    adding addtional styles

--- pokemon-flashcards/src/components/Flashcard.jsx
+++ pokemon-flashcards/src/components/Flashcard.jsx
@@ -11,14 +11,19 @@ const Flashcard = ({pokemon}) => {
         <div className={`flashcard ${isFlipped?'flipped':''}`} onClick={handleFlip}>
         <div className="flashcard-inner">
             <div className="flashcard-front">
+            <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/269px-International_Pok%C3%A9mon_logo.svg.png?20150121202211https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/269px-International_Pok%C3%A9mon_logo.svg.png?20150121202211"
+            alt="Pokémon Logo"
+            className="pokemon-logo"
+          />
             <img src={pokemon.image} alt={pokemon.name} className="flashcard-image"/>
-            <h2>What's that pokemon</h2>
+            <h2>Guess me!!</h2>
             <p>Hint: {pokemon.type}</p>
             </div>
             <div className="flashcard-back">
             <h2>{pokemon.name}</h2>
           <p >Type: {pokemon.type}</p>
-          <p >
+          <p>
             Fun Fact: {pokemon.funFact}
           </p>
           </div>
