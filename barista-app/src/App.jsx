bd005c9d3878525c32ed512cd05496765a0c6288
Krithika Jain <krithikajain@Krithikas-MacBook-Air.local>
2025-02-23 11:21:29 -0500

    adding receipes

--- barista-app/src/App.jsx
+++ barista-app/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import './index.css';
 import BaristaForm from './components/BaristaForm';
+
 
 function App() {
 
   return (
-    <><div className='title-container'>
+    <>
+    <div className='title-container'>
+      <div className="text-center p-4 bg-warning text-dark border-bottom border-dark">
       On my Grind!
-      <h1><p>
+      <h1 className="display-4"><p>
         So you think you can barista? Let's put that to the test...
       </p></h1>
     </div>
-    <BaristaForm /></>
+    </div>
+    <BaristaForm />
+    </>
   )
 }
 
--- barista-app/src/components/BaristaForm.jsx
+++ barista-app/src/components/BaristaForm.jsx
@@ -2,6 +2,18 @@ import React, {Component, useState} from "react";
 import './BaristaForm.css';
 
 const BaristaForm = () => {
+    const [inputs, setInputs] = useState({
+        'temperature': '',
+        'milk': '',
+        'syrup': '',
+        'blended': ''
+      });
+      const ingredients = {
+        'temperature' : ['hot', 'lukewarm', 'cold'],
+        'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
+        'milk': ['cow', 'oat', 'goat', 'almond', 'none'],
+        'blended': ['yes', 'turbo', 'no']
+      }
     const onCheckAnswer = () => {
 
     };
@@ -10,16 +22,16 @@ const BaristaForm = () => {
     };
     return (
         <div>
-            <h2>Hi, I'd like to order a:</h2>
-            <form action="">
+            <h2 className="h4 mb-3">Hi, I'd like to order a:</h2>
+            <form action="" className="mb-3">
 
             </form>
-            <button type="submit" className="button submit"
+            <button type="submit" className="button submit btn btn-primary me-2"
             onClick={onCheckAnswer}>
                 Check Answer
             </button>
             <button type="new-drink-button"
-            className="button submit"
+            className="button submit btn btn-primary me-2"
             onClick={onNewDrink}>
                 New Drink
             </button>
